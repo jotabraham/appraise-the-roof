ffa91d8c5f8d31d170165e7f79a5d565bdde2a9d
khidlay <hello@kerryhidlay.com>
2021-03-12 16:53:32 -0500

    end of day 1

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { RealtorService } from '../realtor.service';
 
 @Component({
   selector: 'app-browse',
   templateUrl: './browse.component.html',
-  styleUrls: ['./browse.component.css']
+  styleUrls: ['./browse.component.css'],
 })
 export class BrowseComponent implements OnInit {
-
-  constructor() { }
+  constructor(private realtorService: RealtorService) {}
 
   ngOnInit(): void {
+    this.realtorService.getListing();
   }
 
+  onSubmit = (form: NgForm) => {
+    console.log(form);
+  };
 }
