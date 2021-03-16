3cba9a2c1dc612ba07c8c570047f6c75e108180e
andrea-s-hoffman <andrea.s.hoffman09@gmail.com>
2021-03-16 13:23:35 -0400

    got guessing math started

--- src/app/game-house-card/game-house-card.component.ts
+++ src/app/game-house-card/game-house-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { RealtorService } from '../realtor.service';
 
 @Component({
   selector: 'app-game-house-card',
@@ -6,12 +8,41 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./game-house-card.component.css']
 })
 export class GameHouseCardComponent implements OnInit {
+  totalScore:number = 0;
 
   @Input() gameCards: any;
 
-  constructor() { }
+  constructor(private realtorService: RealtorService) { }
 
   ngOnInit(): void {
   }
+
+  onGuess = (form: NgForm):number => {
+    let guess:number = form.form.value.guess;
+    let price:number = this.gameCards.price;
+    let number:number = Math.abs(price - guess);
+    let pointsAwarded:number = 0;
+      if (number === 0) {
+         pointsAwarded = 100;
+      } else if (number <= 1000) {
+         pointsAwarded = 80;
+      } else if (number > 1000 && number <=10000) {
+         pointsAwarded = 60;
+      } else if (number > 10000 && number <= 50000) {
+         pointsAwarded = 40;
+      } else if (number > 50000 && number <=100000) {
+         pointsAwarded = 20;
+      } else {
+         pointsAwarded = 5;
+      }
+      console.log("total score", this.totalScore);
+      console.log("guess", guess);
+      console.log("price", price);
+      console.log("number", number);      
+      console.log(`Therefore they were awarded ${pointsAwarded} points`);
+      return this.totalScore += pointsAwarded;
+  }
+
+
 
 }
