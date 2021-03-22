6b50cb997a82c4eadb682c9e3607ca9c029634c9
andrea-s-hoffman <andrea.s.hoffman09@gmail.com>
2021-03-22 16:03:54 -0400

    guess input fix and other css

--- src/app/game-house-card/game-house-card.component.ts
+++ src/app/game-house-card/game-house-card.component.ts
@@ -9,7 +9,8 @@ import { RealtorService } from '../realtor.service';
 })
 export class GameHouseCardComponent implements OnInit {
   @Input() gameCards: any;
-  @Input() favoritesRef!: any[];
+  // @Input() favoritesRef!: any[];
+  @Input() indexRef: number;
   @Output() favoritesEvent = new EventEmitter<any>();
 
   newPointsAwarded: number = 0;
@@ -90,11 +91,19 @@ export class GameHouseCardComponent implements OnInit {
     if (form.form.value.guess !== null) {
       this.numberWithCommas = form.form.value.guess.toLocaleString();
       this.numberWithCommas.replace(/\D/g, ''), 10;
+      this.numberWithCommas = `$${this.numberWithCommas}`
       console.log(this.numberWithCommas);
     } else {
       this.numberWithCommas = null;
     }
 
   }
+
+  // setFocus(name) {    
+  //   const ele = this.aForm.nativeElement[name];    
+  //   if (ele) {
+  //     ele.focus();
+  //   }
+  // }
 
 }
